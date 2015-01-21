2cd22ea81b1bd049f1673041b06336486fa9ce49
Ray Holder <ray@blacklocus.com>
2015-01-20 23:11:32 -0600

    add comments for block strategy stuff

--- src/main/java/com/github/rholder/retry/BlockStrategies.java
+++ src/main/java/com/github/rholder/retry/BlockStrategies.java
@@ -1,13 +1,44 @@
+/*
+ * Copyright 2012-2013 Ray Holder
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.rholder.retry;
 
-public class BlockStrategies {
+import javax.annotation.concurrent.Immutable;
+
+/**
+ * Factory class for {@link BlockStrategy} instances.
+ */
+public final class BlockStrategies {
 
     private static final BlockStrategy THREAD_SLEEP_STRATEGY = new ThreadSleepStrategy();
 
+    private BlockStrategies() {
+    }
+
+    /**
+     * Returns a block strategy that puts the current thread to sleep between
+     * retries.
+     *
+     * @return a block strategy that puts the current thread to sleep between retries
+     */
     public static BlockStrategy threadSleepStrategy() {
         return THREAD_SLEEP_STRATEGY;
     }
 
+    @Immutable
     private static class ThreadSleepStrategy implements BlockStrategy {
 
         @Override
--- src/main/java/com/github/rholder/retry/BlockStrategy.java
+++ src/main/java/com/github/rholder/retry/BlockStrategy.java
@@ -1,7 +1,37 @@
+/*
+ * Copyright 2012-2013 Ray Holder
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.rholder.retry;
 
+/**
+ * This is a strategy used to decide how a retryer should block between retry
+ * attempts. Normally this is just a Thread.sleep(), but implementations can be
+ * something more elaborate if desired.
+ */
 public interface BlockStrategy {
 
+    /**
+     * Attempt to block for the designated amount of time. Implementations
+     * that don't block or otherwise delay the processing from within this
+     * method for the given sleep duration can significantly modify the behavior
+     * of any configured {@link com.github.rholder.retry.WaitStrategy}. Caution
+     * is advised when generating your own implementations.
+     *
+     * @param sleepTime the computed sleep duration in milliseconds
+     * @throws InterruptedException
+     */
     void block(long sleepTime) throws InterruptedException;
-
 }
